7406a9807b07d2ad960c49ecb7cedb6957130412
RS <roboszyn@gmail.com>
2017-03-07 03:43:19 +0000

    fixing finction and adding var's

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@ var h = prompt('Podaj wysokość trójkąta');
 
 
 function getTriangleArea (a, h) {
-	if ((a <= 0) || (h <= 0)) {return alert('Nieprawidłowe dane');}
-	return alert('pole Δ = ' +  a * h / 2);
+	if ((a <= 0) || (h <= 0)) {
+	  alert('Nieprawidłowe dane');
+	} else { 
+	  	alert('pole Δ = ' +  a * h / 2);
+	}
 }
-console.log( getTriangleArea(a,h))
+
+console.log(getTriangleArea (a, h));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 12);
+var triangle3Area = getTriangleArea(4, 9);
